b459a0cb5ae64d090ad8fb4e1e1fee007de2faae
ericazhang17 <zzhang4@wellesley.edu>
2021-07-11 02:34:59 -0400

    binary search in array, matrix

--- src/BinarySearch1.java
+++ src/BinarySearch1.java
@@ -1,14 +1,16 @@
 public class BinarySearch1 {
-	public int binarySearch (int[]a, int target) {
+	public int binarySearch1 (int[] array, int target) {
+		if (array == null || array.length == 0) {
+			return -1;
+		}
 		int left = 0;
-		int right = a.length - 1;
-
-		while (left <= right ) { // one element array can enter loop
-			int mid = left + (right - left)/2;
-			if (a[mid] == target) {
+		int right = array.length - 1;
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+			if (array[mid] == target) {
 				return mid;
-			} else if (a[mid] < target) {
-				left = mid + 1; //avoid dead loop
+			} else if (array[mid] < target) {
+				left = mid + 1;
 			} else {
 				right = mid - 1;
 			}
@@ -16,26 +18,40 @@ public class BinarySearch1 {
 		return -1;
 	}
 
+	public int[] search (int[][] matrix, int target) {
+		if (matrix == null && matrix[0] == null) {
+			int[] result = {-1,-1};
+			return result;
+		}
+		int r = matrix.length;
+		int c = matrix[0].length;
+		int left = 0;
+		int right = r * c - 1;
+		while (left <= right) {
+			int mid = left + (right - left) /2;
+			int row = mid / c;
+			int col = mid % c;
+			if (matrix[row][col] == target) {
+				int[] result = {row, col};
+				return result;
+			} else if (matrix[row][col] < target) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+		int[] result = {-1,-1};
+		return result;
+	}
+
+
+
 	public static void main(String[] args) {
 		int[] test1 = {1,2};
 		int[][] test2 = {{1,3,4,6},{7,9,10,11},{14,17,19,20}};
 		int[][] test3 = { {1, 2, 3}, {4, 5, 7}, {8, 9, 10} };
 		BinarySearch1 p = new BinarySearch1();
-
-        System.out.println(p.binarySearch(test1,4));
-//		System.out.println(p.ifFind(test2,11));
-//		System.out.println(Arrays.toString(p.search(test3, 6))); // print arrays this way
-//		System.out.println(p.binarySearch2(new int[]{1,2,3,8,9},0,4,9));
-//		System.out.println(p.closest(new int[]{1,2,3},2));
-//		System.out.println(p.firstOccur(new int[]{1,2,2,2,3}, 2));
-//		System.out.println(p.firstOccur(new int[]{2,2,2,2,2,2,8,13,13}, 2));
-//		System.out.println(p.lastOccur(new int[]{2,2,2,2,2,2,8,13,13}, 2));
-//		System.out.println(Arrays.toString(p.kClosest(new int[]{1,4,6,8}, 3, 3)));
-//		System.out.println(Arrays.toString(p.kClosest(new int[]{1,2,3}, 2, 3)));
-//		System.out.println(Arrays.toString(p.kClosest(new int[]{0,0,1,2,3,3,4,7,7,8}, 3, 3)));
-//		System.out.println(p.smallestElementLargerThanTarget(new int[]{2,10,13,15,19,20,27,29,30,34,44,45,49,51,55,60,64}, 1));
-//		System.out.println(p.search([1],2)); // 到底怎么pass in dictionary??
-
+		System.out.println(p.binarySearch1(test1, 2));
 	}
 
 }
